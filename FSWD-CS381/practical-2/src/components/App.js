cc625365ea9e85604e69a38ed6ea0e40fa1acf2f
Kaushal Patel <55987371+mrkaushal@users.noreply.github.com>
2022-07-23 08:38:44 +0000

    Working on FSWD Practical 2

--- FSWD-CS381/practical-2/src/components/App.js
+++ FSWD-CS381/practical-2/src/components/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter ,Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Home from './Home/Home';
 import About from './About/About';
 import Contact from './Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/contact" element={<Contact />} />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/contact" element={<Contact/>}/>
         </Routes>
-      </BrowserRouter>
-    </div>
+    </BrowserRouter>
   );
 }
 
