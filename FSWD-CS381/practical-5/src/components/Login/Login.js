921f17c196e96b55f4dddd3a87c3c43845d2a394
Kaushal Patel <55987371+mrkaushal@users.noreply.github.com>
2022-08-04 01:48:26 +0000

    FSWD Practical 5 -- Instagram Login Working

--- FSWD-CS381/practical-5/src/components/Login/Login.js
+++ FSWD-CS381/practical-5/src/components/Login/Login.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import homepage from '../../assests/images/homepage.webp';
+import React from 'react'
+import TextField from '@mui/material/TextField';
+import Auth from './Auth';
+import { Link } from 'react-router-dom';
 
-const Auth = ({ children }) => {
+const Login = () => {
+
     return (
-        <div className="w-full h-full">
-
-            <div className="flex w-full h-screen md:w-2/3 py-8 mx-auto">
-
-                <div className="hidden md:block bg-[url('https://www.instagram.com/static/images/homepage/phones/home-phones.png/1dc085cdb87d.png')] my-10 h-full bg-no-repeat">
-                    <img draggable="false" className="mr-[80px] mt-[1.8rem] ml-[155px]" src={homepage} alt="homepage" />
+        <>
+            <Auth>
+                <div className="bg-white border flex flex-col gap-2 p-4 pt-10">
+                    <img draggable="false" className="mx-auto h-30 w-36 object-contain" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
+                    <form className="flex flex-col justify-center items-center gap-3 m-3 md:m-8">
+                        <TextField
+                            label="Email/Username"
+                            type="text"
+                            required
+                            size="small"
+                            fullWidth
+                        />
+                        <TextField
+                            label="Password"
+                            type="password"
+                            required
+                            size="small"
+                            fullWidth
+                        />
+                        <button type="submit" className="bg-primary-blue font-medium py-2 rounded text-white w-full">Log In</button>
+                        <span className="my-3 text-gray-500">OR</span>
+                        <Link to="/password/forgot" className="text-sm font-medium text-blue-800">Forgot password?</Link>
+                    </form>
                 </div>
 
-                <div className="flex flex-col gap-3 w-full md:w-2/5">
-
-                    {children}
-
-                    <p className="text-center text-sm my-2">Get the app.</p>
-                    <div className="flex gap-3 justify-center" >
-                        <img draggable="false" width="130px" src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png" alt="appstore" />
-                        <img draggable="false" width="130px" src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png" alt="playstore" />
-                    </div>
-
+                <div className="bg-white border p-5 text-center">
+                    <span>Don't have an account? <Link to="/register" className="text-primary-blue">Sign up</Link></span>
                 </div>
-            </div>
-        </div>
+            </Auth>
+        </>
     )
 }
 
-export default Auth
+export default Login
