9c0f278d67fa683185499f493e533017873f348e
Kaushal Patel <55987371+mrkaushal@users.noreply.github.com>
2022-11-10 05:41:13 +0000

    Practical 5

---
+++ OS-CS350/practical-5/pr5b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+int main()
+{
+int pid;
+pid=vfork();
+if(pid==0){
+cout<<"\nParent Process id : "<<getpid()<<endl;
+cout<<"\nChild Process with parent id : "<<getppid()<<endl;
+}
+return 0;
+}
